fdb672f0b1b5538e3cae2c425cf375d76ca9d04c
Mateusz Najsztub <mateusz.najsztub@gmail.com>
2016-05-06 18:59:32 +0200

    Added mongodb test

--- flaskapp.py
+++ flaskapp.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import request
+import pymongo
 
 app = Flask(__name__)
 
@@ -10,6 +11,16 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     return "This is the application mynote & I'm alive"
+
+@app.route("/mongo/test")
+def mongo_test():
+    #setup the connection
+    conn = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_URL'])
+    db = conn.parks
+    
+    #query the DB for all the parkpoints
+    result = db.parkpoints.find()
+    return "There are %s data points in MongoDB base"%result
 
 if __name__ == "__main__":
     app.run(debug = "True")
