cf5ae661a23197aad43f490f012cdce7bb8881cb
Mateusz Najsztub <mateusz.najsztub@gmail.com>
2016-05-06 19:51:01 +0200

    MongoDB prints first observation

--- flaskapp.py
+++ flaskapp.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 from flask import Flask
 from flask import render_template
@@ -12,15 +13,25 @@ app = Flask(__name__)
 def index():
     return "This is the application mynote & I'm alive"
 
-@app.route("/mongo/test")
+@app.route("/mongo/test", methods = ['GET', 'POST'])
 def mongo_test():
     #setup the connection
     conn = pymongo.MongoClient(os.environ['OPENSHIFT_MONGODB_DB_URL'])
     db = conn.python
     
     #query the DB for all the parkpoints
-    result = db.szczecin.count()
-    return "There are %s data points in MongoDB base"%result
+    obsNum = db.szczecin.count()
+    obs = db.szczecin.find().limit(1)
+    outstr = ""
+    for k in obs[0]:
+        outstr += "%s,\t %s\n"%(k, obs[0][k])
+    
+    
+    info = {"num": obsNum,
+            "content": outstr
+            }
+    
+    return render_template("mongo_test.html", info = info)
 
 if __name__ == "__main__":
     app.run(debug = "True")
