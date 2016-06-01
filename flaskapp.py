fac97bb91ebbf41f45171666b5710cfe0fda1f40
Mateusz Najsztub <mateusz.najsztub@gmail.com>
2016-06-01 21:59:25 +0200

    Back to old key

--- flaskapp.py
+++ flaskapp.py
@@ -19,8 +19,7 @@ def mongo_map():
 @app.route("/mongo/data")
 def mongo_data():
     #setup the connection os.environ['OPENSHIFT_MONGODB_DB_URL'])
-    #key = "mongodb://localhost:27017/test"
-    key = os.environ['OPENSHIFT_MONGODB_DB_URL']
+    key = "mongodb://localhost:27017/test"
     conn = pymongo.MongoClient(key)
     db = conn.python
     
@@ -52,7 +51,7 @@ def add_message():
     except TypeError:
         return "Error; none or invalid query given", 500
     #setup the connection
-    key = os.environ['OPENSHIFT_MONGODB_DB_URL']
+    key = "mongodb://localhost:27017/test"
     conn = pymongo.MongoClient(key)
 
     #conn = pymongo.MongoClient(os.environ['OPENSHIFT_MONGODB_DB_URL'])
