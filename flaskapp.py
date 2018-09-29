19bf89ad9ca61069f875d45d44a374fe70172051
Mateusz Najsztub <mateusz.najsztub@gmail.com>
2018-09-29 15:15:42 +0200

    Removed MongoDB calls

--- flaskapp.py
+++ flaskapp.py
@@ -27,49 +27,6 @@ def about_page():
     return render_template("about.html")
 
 
-@app.route("/mongo/data")
-def mongo_data():
-    # setup the connection os.environ['OPENSHIFT_MONGODB_DB_URL'])
-    key = "mongodb://localhost:27017/test"
-    conn = pymongo.MongoClient(key)
-    db = conn.python
-
-    # query the DB for all the coordinates
-    coords = db.szczecin.find({'town': 'Szczecin',
-                               'data_lat': {'$gt': 0.0, "$lt": 54.0},
-                               'data_lon': {'$gt': 14.0, '$lt': 15.0}},
-                              {'data_lat': 1, 'data_lon': 1,
-                               'price': 1, 'pow': 1, '_id': 1})
-    # json_coords = [dumps(c) for c in coords]
-    conn.close()
-    return Response(
-        dumps(coords),
-        mimetype='application/json'
-    )
-
-
-@app.route('/mongo/item', methods=['GET', 'POST'])
-def add_message():
-    content = request.json
-    # print content
-
-    # Construct MongoDB query
-    try:
-        qu = [ObjectId(el['$oid']) for el in content]
-    except TypeError:
-        return "Error; none or invalid query given", 500
-    # setup the connection
-    key = "mongodb://localhost:27017/test"
-    conn = pymongo.MongoClient(key)
-
-    # conn = pymongo.MongoClient(os.environ['OPENSHIFT_MONGODB_DB_URL'])
-    db = conn.python
-
-    items = db.szczecin.find({'_id': {'$in': qu}}).limit(50)
-    conn.close()
-    return Response(dumps(items), mimetype='application/json')
-
-
 @app.route('/dates')
 def get_dates():
     conn = psycopg2.connect(app.config['SQLALCHEMY_DATABASE_URI'])
